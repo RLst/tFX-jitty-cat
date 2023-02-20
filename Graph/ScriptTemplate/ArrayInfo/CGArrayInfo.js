3b27f555c44f10307d8e3371a8d25e2df3e72306
Tony Le <letony@icloud.com>
2023-02-20 15:44:05 +0700

    Implement V gesture bling effect; Update nose color change cycling function based on screen tap

---
+++ Graph/ScriptTemplate/ArrayInfo/CGArrayInfo.js
@@ -0,0 +1,19 @@
+
+"use strict";
+const Amaz = effect.Amaz;
+const { BaseNode } = require('./BaseNode');
+class CGArrayInfo extends BaseNode {
+    constructor() {
+        super();
+    }
+
+    getOutput() {
+        const array = this.inputs[0]();
+        if(array === null || array === undefined){
+            return;
+        }
+        return array.length;
+    }
+}
+exports.CGArrayInfo = CGArrayInfo;
+    
